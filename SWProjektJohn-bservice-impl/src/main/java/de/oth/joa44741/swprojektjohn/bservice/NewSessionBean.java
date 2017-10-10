a2ff7abf1d070022757601267bffdd5c2f3b0d93
Andreas John <john_andreas@gmx.net>
2017-10-10 14:02:19 +0200

    initial commit

---
+++ SWProjektJohn-bservice-impl/src/main/java/de/oth/joa44741/swprojektjohn/bservice/NewSessionBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.oth.joa44741.swprojektjohn.bservice;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author martin
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
